ff83adfaffe864b02b95199d9e579dcb726622b5
driatic <fredrik.hasselgren@prototyp.se>
2024-01-18 10:25:46 +0100

    Feat: Mock ContecOS data in adapter

--- src/services/property-info-service/adapters/contech-os-adapter.ts
+++ src/services/property-info-service/adapters/contech-os-adapter.ts
@@ -1,124 +1,36 @@
-import axios from 'axios'
 import { RentalProperty, RoomType } from '../../../common/types'
-import Config from '../../../common/config'
-
-const FK_SIZE = "Lägenhetsyta";
-const FK_RENTAL_PROPERTY_TYPE = "Lägenhetstyp";
-const FK_APARTMENT_NUMBER = "Lägenhetsnummer";
-const FK_ADDRESS = "Lägenhetsadress";
-const FK_ROOM_TYPE = "Rum i Lägenhet"
 
 const getRentalProperty = async (
   rentalPropertyId: string
 ): Promise<RentalProperty> => {
-  const url = `${Config.contechOs.url}`
-  const response = await axios({
-    method: 'post',
-    url: url,
-    headers: {
-      'Ocp-Apim-Subscription-Key': 'a372e65dd6204183990487d891cb6b37',
-    },
-    data: {
-      targetTitle: rentalPropertyId,
-    },
-  })
-
-  const json = response.data
-  const propertyId = json[0].target.title
-
-  let apartmentNumber: number = 0;
-  let size: number = 0;
-  let address: {
-    street: string;
-    number: string;
-    postalCode: string;
-    city: string;
-  } = {
-    street: '',
-    number: '',
-    postalCode: '',
-    city: '',
-  };
-  let rentalPropertyType: string = '';
-  let type: string = '';
-  let otherInfo: undefined = undefined;
-  let lastUpdated: undefined = undefined;
-
-  json[0].links.forEach((link: { sources: any; linkTitle: any }) => {
-    const sources = link.sources;
-    
-    if (sources.length > 0) {
-      if (sources.length === 1) {
-        const source = sources[0];
-        const value = source.title;
-        const foreignKey = source.parentTitle;
-        
-        if (foreignKey === FK_SIZE) {
-          size = value;
-        }
-        if (foreignKey === FK_RENTAL_PROPERTY_TYPE) {
-          rentalPropertyType = value;
-        }
-        if (foreignKey === FK_APARTMENT_NUMBER) {
-          apartmentNumber = value;
-        }
-        if (foreignKey === FK_ADDRESS) {
-          const parts = value.match(/^([\s\S]+?)\s(\d+)$/); // Split into string and number
-          address.street = parts[1];
-          address.number = parts[2];
-        }
-      }
-    }
-  });
-  
   return {
-    rentalPropertyId: propertyId,
-    apartmentNumber: apartmentNumber,
-    size: size,
-    address: address,
-    rentalPropertyType: rentalPropertyType,
-    type: type,
+    rentalPropertyId: '102-008-03-0202', //property-info.contact.RentalPropertyId
+    apartmentNumber: 1207, //ContecOS data?
+    size: 85, //contecOS data?
+    address: {
+      street: 'Gatvägen', //property-info.contact.Street
+      number: '56', //property-info.contact.StreetNumber
+      postalCode: '72266', //property-info.contact.PostalCode
+      city: 'Västerås', //property-info.contact.City
+  },
+    //below fields needs to be investigated more, might be taken from ContecOS?
+    rentalPropertyType: 'Bostadskontrakt', //property-info.lease.Type
+    type: 'Kontraktsinnehavare', //property-info.contact.Type
     additionsIncludedInRent: '',
-    otherInfo: otherInfo,
-    lastUpdated: lastUpdated,
+    otherInfo: '',
+    lastUpdated: new Date('2024-01-17'),
   }
 }
 
 const getRoomTypes = async (aparmentId: string): Promise<Array<RoomType>> => {
-  const roomTypes: Array<RoomType> = [];
-  const url = `${Config.contechOs.url}`
-  const response = await axios({
-    method: 'post',
-    url: url,
-    headers: {
-      'Ocp-Apim-Subscription-Key': 'a372e65dd6204183990487d891cb6b37',
-    },
-    data: {
-      targetTitle: aparmentId,
-    },
-  })
-  response.data[0].links.forEach((link: { sources: any; linkTitle: any }) => {
-    link.sources.forEach((source: { parentTitle: any; id: any; title: any; }) => {
-      const foreignKey = source.parentTitle;
-      if (foreignKey === FK_ROOM_TYPE) {
-        roomTypes.push({
-          roomTypeId: source.title,
-          name: source.title
-        })
-      }
-    });
-  });
+  const roomTypes: Array<RoomType> = [
+    { roomTypeId: 'BADRUM', name: 'BADRUM'},
+    { roomTypeId: 'VARDAGSRUM', name: 'VARDAGSRUM'},
+    { roomTypeId: 'RUM 1', name: 'RUM 1'},
+    { roomTypeId: 'KÖK', name: 'KÖK'},
+  ];
+
   return roomTypes;
 }
 
-const getRoomType = async (
-  aparmentId: string,
-  roomTypeId: string
-): Promise<RoomType | undefined> => {
-  const roomTypes = await getRoomTypes(aparmentId)
-  return roomTypes.find(
-    (roomType: RoomType) => roomType.roomTypeId == roomTypeId
-  )
-}
-
-export { getRoomTypes, getRoomType, getRentalProperty }
+export { getRoomTypes, getRentalProperty }
