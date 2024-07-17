91896f85795ce0d737ac8dc7a6accc6fc2366685
driatic <fredrik.hasselgren@prototyp.se>
2024-07-17 15:02:06 +0200

    feat: 1537 add swagger docs

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,12 @@
 import KoaRouter from '@koa/router'
 import { routes as propertyInfoRoutes } from './services/property-info-service'
 import { routes as healthRoutes } from './services/health-service'
+import { routes as swagggerRoutes } from './services/swagger'
 
 const router = new KoaRouter()
 
-propertyInfoRoutes(router)
 healthRoutes(router)
+propertyInfoRoutes(router)
+swagggerRoutes(router)
 
 export default router
