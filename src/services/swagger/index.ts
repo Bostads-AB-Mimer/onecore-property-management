1eccace4e2ce877913ae1ae86e7bc55c32c7cbad
driatic <fredrik.hasselgren@prototyp.se>
2024-08-13 10:53:42 +0200

    fix: 1537 handle swagger spec by base path

--- src/services/swagger/index.ts
+++ src/services/swagger/index.ts
@@ -1,25 +1,12 @@
 import KoaRouter from '@koa/router'
 import swaggerJsdoc from 'swagger-jsdoc'
+import { swaggerSpec } from '../../swagger'
+
+const swaggerOptions = swaggerJsdoc(swaggerSpec)
 
 export const routes = (router: KoaRouter) => {
-  const options = {
-    definition: {
-      openapi: '3.0.0',
-      info: {
-        title: 'onecore-property-management',
-        version: '1.0.0',
-      },
-    },
-    apis: [
-      './src/services/health-service/*.ts',
-      './src/services/property-info-service/*.ts',
-    ],
-  }
-
-  const swaggerSpec = swaggerJsdoc(options)
-
-  router.get('/swagger.json', async function (ctx) {
+  router.get('/swagger.json', async (ctx) => {
     ctx.set('Content-Type', 'application/json')
-    ctx.body = swaggerSpec
+    ctx.body = swaggerOptions
   })
 }
